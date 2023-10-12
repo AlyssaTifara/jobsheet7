b4c572412014da394554cc7ee29d46906330049e
Alyssa Tifara Yuwono <tissaalyssa@gmail.com>
2023-10-12 12:57:05 +0700

    soal_percobaan1

--- ForKelipatan03.java
+++ ForKelipatan03.java
@@ -3,6 +3,8 @@ public class ForKelipatan03{
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int kelipatan, jumlah, counter;
+        float rata;
+        rata = 0;
         jumlah = 0;
         counter = 0;
 
@@ -13,10 +15,12 @@ public class ForKelipatan03{
             if (i % kelipatan == 0) {
                 jumlah += i;
                 counter++;
+                rata = jumlah / counter;
 
                 System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
                 System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, jumlah);
-                
+                System.out.printf("Rata-rata seluruh bilangan kelipatan %d adalah %.3f\n", kelipatan, rata);
+
 
             }
         }
