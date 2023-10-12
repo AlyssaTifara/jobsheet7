0a9892319807ec4253546086e181cc3458246ff9
Alyssa Tifara Yuwono <tissaalyssa@gmail.com>
2023-10-12 12:56:27 +0700

    soal_percobaan1

---
+++ WhileGaji03.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class WhileGaji03 {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int jumlahKaryawan, jumlahJamLembur;
+        double gajiLembur, totalGajiLembur;
+        String jabatan;
+        gajiLembur = 0;
+        totalGajiLembur = 0;
+
+        System.out.print("Masukkan jumlah karyawan")
+    }
+}
