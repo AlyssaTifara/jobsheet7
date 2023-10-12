68831eb4252cddcec538569e2bc925d90a50e116
Alyssa Tifara Yuwono <tissaalyssa@gmail.com>
2023-10-12 13:14:48 +0700

    percobaan2

--- WhileGaji03.java
+++ WhileGaji03.java
@@ -8,6 +8,28 @@ public class WhileGaji03 {
         gajiLembur = 0;
         totalGajiLembur = 0;
 
-        System.out.print("Masukkan jumlah karyawan")
+        System.out.print("Masukkan jumlah karyawan");
+        jumlahKaryawan = scan.nextInt();
+
+        int i = 0;
+        while (i < jumlahKaryawan) {
+            System.out.println("Pilihan jabatan - Direktur, Manajer, Karyawan");
+            System.out.print("Masukkan jabatan karyawan ke-" + (i+1) + ": ");
+            jabatan = scan.next();
+            System.out.print("Masukkan jumlah jam lembur: ");
+            jumlahJamLembur = scan.nextInt();
+            i++;
+
+            if (jabatan.equalsIgnoreCase("direktur")){
+                continue;
+            } else if (jabatan.equalsIgnoreCase("manajer")) {
+                gajiLembur = jumlahJamLembur * 100000;
+            } else if (jabatan.equalsIgnoreCase("karyawan")) {
+                gajiLembur = jumlahJamLembur *75000;
+            }
+            totalGajiLembur += gajiLembur;
+        }
+
+        System.out.println("Total gaji lembur: " + totalGajiLembur);
     }
 }
